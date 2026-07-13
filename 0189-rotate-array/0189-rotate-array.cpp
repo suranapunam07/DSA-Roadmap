c96c83925882e22bb1d1bf8124fc589cf9c010be
suranapunam07 <suranapunam07@gmail.com>
2026-07-13 22:43:43 +0530

    Time: 0 ms (100%), Space: 29.6 MB (45.75%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -11,3 +11,15 @@ public:
         reverse(nums.begin() + k, nums.end());    
     }
 };
+
+/* Extra Array [T.C=O(n) & S.C=O(n)]
+
+k %= n;
+vector<int> ans(n);
+
+for(int i < 0; i < n; i++)
+{
+    ans[(i + k) % n] =nums[i];
+}
+nums = ans;
+*/
