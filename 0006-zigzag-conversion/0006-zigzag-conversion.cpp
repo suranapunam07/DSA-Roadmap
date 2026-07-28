4898cf5ceaec5279e2e619581cd125fb4daf247f
suranapunam07 <suranapunam07@gmail.com>
2026-07-28 09:48:46 +0530

    Time: 0 ms (100%), Space: 10.9 MB (91.15%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,8 +1,43 @@
 class Solution {
 public:
-    string convert(string s, int numRows) {
+    string convert(string s, int numRows) 
+    {
+       // T.C=O(n) S.C=O(1)
 
         if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+
+        string ans = "";
+
+        int cycleLength = 2 * (numRows - 1);
+
+        for (int row = 0; row < numRows; row++) {
+
+            for (int j = row; j < s.length(); j += cycleLength) {
+
+                // Vertical character
+                ans += s[j];
+
+                // Diagonal character (only for middle rows)
+                int diagonal = j + cycleLength - 2 * row;
+
+                if (row != 0 &&
+                    row != numRows - 1 &&
+                    diagonal < s.length()) {
+
+                    ans += s[diagonal];
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+/*
+T.C=O(N) AND S.C=0(N)
+if (numRows == 1 || numRows >= s.length()) {
             return s;
         }
 
@@ -37,5 +72,4 @@ public:
         }
 
         return ans;
-    }
-};
+*/
