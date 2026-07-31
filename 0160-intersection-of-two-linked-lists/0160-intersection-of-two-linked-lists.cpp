290863179137fcde4ddd430bc5a87a3a5eb46a0c
suranapunam07 <suranapunam07@gmail.com>
2026-07-31 10:11:15 +0530

    Time: 56 ms (59.53%), Space: 23.9 MB (86.84%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,22 +8,52 @@
  */
 class Solution {
 public:
+    int length(ListNode* head)
+    {
+        int count = 0;
+
+        while(head)
+        {
+            count++;
+            head = head->next;
+        }
+        return count;
+    }
+
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
     {
-        unordered_set<ListNode*> st;
+        int lenA = length(headA);
+        int lenB = length(headB);
 
-        while(headA)
+        ListNode* p1 = headA;
+        ListNode* p2 = headB;
+
+        if(lenA > lenB)
         {
-            st.insert(headA);
-            headA = headA->next;
-        }
-        while(headB)
-        {
-            if(st.find(headB) != st.end())
+            int diff = lenA - lenB;
+            
+            while(diff--)
             {
-                return headB;
+                p1 = p1->next;
             }
-            headB = headB->next;
+        } 
+        else
+        {
+            int diff = lenB - lenA;
+
+            while(diff--)
+            {
+                p2 = p2->next;
+            }
+        }
+        while(p1 && p2)
+        {
+            if(p1 == p2)
+            {
+                return p1;
+            }
+            p1 = p1->next;
+            p2 = p2->next;
         }
         return NULL;
     }
@@ -44,6 +74,25 @@ ListNode* tempA = headA;
                 tempB = tempB->next;
             }
             tempA = tempA->next;
+        }
+        return NULL;
+*/
+
+/*
+unordered_set<ListNode*> st;
+
+        while(headA)
+        {
+            st.insert(headA);
+            headA = headA->next;
+        }
+        while(headB)
+        {
+            if(st.find(headB) != st.end())
+            {
+                return headB;
+            }
+            headB = headB->next;
         }
         return NULL;
 */
