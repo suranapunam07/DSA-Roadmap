debe38af5912ef23b18a0ad7745ecb1cd0b40e9a
suranapunam07 <suranapunam07@gmail.com>
2026-08-01 23:07:43 +0530

    Time: 3 ms (28.02%), Space: 95.8 MB (25.92%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -44,3 +44,75 @@ public:
         }
     }
 };
+
+/*
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+        // Always perform binary search on the smaller array
+        if (nums1.size() > nums2.size())
+            return findMedianSortedArrays(nums2, nums1);
+
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+
+        int start = 0;
+        int end = n1;
+
+        while (start <= end) {
+
+            int cut1 = (start + end) / 2;
+            int cut2 = (n1 + n2 + 1) / 2 - cut1;
+
+            int left1, left2, right1, right2;
+
+            // left1
+            if (cut1 == 0)
+                left1 = INT_MIN;
+            else
+                left1 = nums1[cut1 - 1];
+
+            // right1
+            if (cut1 == n1)
+                right1 = INT_MAX;
+            else
+                right1 = nums1[cut1];
+
+            // left2
+            if (cut2 == 0)
+                left2 = INT_MIN;
+            else
+                left2 = nums2[cut2 - 1];
+
+            // right2
+            if (cut2 == n2)
+                right2 = INT_MAX;
+            else
+                right2 = nums2[cut2];
+
+            // Correct partition
+            if (left1 <= right2 && left2 <= right1) {
+
+                if ((n1 + n2) % 2 == 0) {
+                    return (max(left1, left2) + min(right1, right2)) / 2.0;
+                } else {
+                    return max(left1, left2);
+                }
+            }
+
+            // Move left
+            else if (left1 > right2) {
+                end = cut1 - 1;
+            }
+
+            // Move right
+            else {
+                start = cut1 + 1;
+            }
+        }
+
+        return 0.0;
+    }
+};
+*/
