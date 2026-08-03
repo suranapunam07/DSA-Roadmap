c5dc31f54441d8356955f71b5b44e155ebac4776
suranapunam07 <suranapunam07@gmail.com>
2026-08-03 11:22:17 +0530

    Time: 0 ms (100%), Space: 15.5 MB (64.17%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -1,5 +1,32 @@
 class Solution {
 public:
+    ListNode* oddEvenList(ListNode* head) 
+    {
+        if(head == NULL || head->next == NULL)
+        
+            return head;
+
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even;
+
+        while(even != NULL && even->next != NULL)
+        {
+            odd->next = even->next;
+            odd = odd->next;
+
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;    
+        
+        return head;
+    }
+};
+
+/*
+class Solution {
+public:
     ListNode* oddEvenList(ListNode* head) {
 
         if (head == nullptr)
@@ -34,3 +61,4 @@ public:
         return oddDummy->next;
     }
 };
+*/
