183f59290245e4214333313eb85d51cae8064e21
suranapunam07 <suranapunam07@gmail.com>
2026-08-04 10:14:23 +0530

    Time: 7 ms (95.35%), Space: 57.1 MB (82.91%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -1,7 +1,55 @@
 class Solution {
 public:
+    ListNode* findMid(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+
+    ListNode* merge(ListNode* left, ListNode* right) {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        while (left && right) {
+            if (left->val <= right->val) {
+                tail->next = left;
+                left = left->next;
+            } else {
+                tail->next = right;
+                right = right->next;
+            }
+            tail = tail->next;
+        }
+
+        if (left) tail->next = left;
+        else tail->next = right;
+
+        return dummy.next;
+    }
+
     ListNode* sortList(ListNode* head) {
-        if (!head) return head;
+        if (!head || !head->next)
+            return head;
+
+        ListNode* mid = findMid(head);
+        ListNode* right = mid->next;
+        mid->next = nullptr;   // Split the list
+
+        ListNode* left = sortList(head);
+        right = sortList(right);
+
+        return merge(left, right);
+    }
+};
+
+/*
+if (!head) return head;
 
         vector<int> arr;
 
@@ -21,5 +69,4 @@ public:
         }
 
         return head;
-    }
-};
+*/
