ae85bcdef0021be472453d9149c5f500cb36999d
suranapunam07 <suranapunam07@gmail.com>
2026-08-31 23:24:51 +0530

    Time: 0 ms (100%), Space: 10.1 MB (86.55%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int value(char ch)
+    {
+        if(ch == 'I') return 1;
+        if(ch == 'V') return 5;
+        if(ch == 'X') return 10;
+        if(ch == 'L') return 50;
+        if(ch == 'C') return 100;
+        if(ch == 'D') return 500;
+        
+        return 1000;
+    }
+    int romanToInt(string s) 
+    {
+        int n = s.length();
+        int ans = 0;
+
+        for(int i = 0; i<n; i++)
+        {
+            if(i + 1 < n && value(s[i]) < value(s[i + 1]))
+            {
+                ans = ans - value(s[i]);
+            }
+            else
+            {
+                ans = ans + value(s[i]);
+            }
+        }
+        return ans;  
+    }
+};
