5a389a1db94e5f7d474826831125e6f1b9875077
suranapunam07 <suranapunam07@gmail.com>
2026-09-04 21:27:29 +0530

    Time: 0 ms (100%), Space: 31 MB (72.92%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -3,6 +3,33 @@ public:
     int firstStableIndex(vector<int>& nums, int k) 
     {
         int n = nums.size();
+
+        vector<int> suffixMin(n);
+
+        suffixMin[n - 1] = nums[n - 1];
+
+        for(int i = n - 2; i >= 0; i--)
+        {
+            suffixMin[i] = min(nums[i],suffixMin[i + 1]);
+        }
+
+        int mx = nums[0];
+
+        for(int i = 0; i < n; i++)
+        {
+            mx = max(mx,nums[i]);
+
+            if(mx - suffixMin[i] <= k)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+/*
+      int n = nums.size();
         
         vector<int> suffixMin(n);
 
@@ -28,4 +55,4 @@ public:
         }
         return -1;
     }
-};
+*/
