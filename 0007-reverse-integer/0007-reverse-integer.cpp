c7071f9ca666a8e013718c18cd49ac82a2eda22c
Punam Surana <suranapunam07@gmail.com>
2026-09-22 23:40:37 +0530

    Time: 2 ms (38.15%), Space: 8.5 MB (54.21%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverse(int n) 
+    {
+        int revNum = 0;
+
+        while(n != 0)
+        {
+            int dig = n % 10;
+            if(revNum > INT_MAX/10 || revNum < INT_MIN/10) //line bcoz of contraints
+            {
+                return 0;
+            }
+            revNum = revNum * 10 + dig;
+            n = n / 10;
+        }
+        return revNum;  
+    }
+};
